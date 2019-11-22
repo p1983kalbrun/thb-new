08ab17986e2aa71eb5b47f9af6f87a577b485a8c
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-11-22 16:26:58 +0000

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/ntrs/root/ChrisdemoApplication.java
@@ -0,0 +1,12 @@
+package com.ntrs.root;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisdemoApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(ChrisdemoApplication.class, args);
+	}
+
+}
